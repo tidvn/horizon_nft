42fc4c9e7d95f21f9784c0143ef7c2da2e52cba4
tidvn <tiendung0325@gmail.com>
2024-07-25 13:56:43 +0700

    edit name

--- public/images/index.ts
+++ public/images/index.ts
@@ -4,7 +4,7 @@ const images = {
     gero: require("./wallets/gero.webp"),
     typhon: require("./wallets/typhon.svg"),
     flint: require("./wallets/flint.svg"),
-    vespr: require("./wallets/unnamed.webp"),
+    vespr: require("./wallets/vespr.webp"),
     lace: require("./wallets/lace.png"),
     yoroi: require("./wallets/yoroi.png"),
     nufi: require("./wallets/nufi.png"),
