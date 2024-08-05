b375b26d517b4304c38945673709490c31fbd3d0
tidvn <tiendung0325@gmail.com>
2024-08-05 16:15:29 +0000

    done

--- src/components/common/connect-wallet/index.tsx
+++ src/components/common/connect-wallet/index.tsx
@@ -1,10 +1,14 @@
 import dynamic from "next/dynamic"
 
+// const ConnectWallet = dynamic(
+//   async () => (await import("@use-cardano")).CardanoWalletSelector,
+//   { ssr: false }
+// )
 const ConnectWallet = dynamic(
-  async () => (await import("@use-cardano")).CardanoWalletSelector,
-  { ssr: false }
-)
-
+    async () => (await import("./wallet-connect")),
+    { ssr: false }
+  )
+  
 export default function ConnectWalletButton() {
   return <ConnectWallet />
 }
--- use-cardano/index.ts
+++ use-cardano/index.ts
@@ -1,4 +1,5 @@
 import { toNetworkId, toNetworkName } from "./lib/network-dictionary"
+import { shortAddress } from "./lib/short-address"
 import { supportedWalletProviders } from "./lib/supported-wallet-providers"
 import { hasErrorCode } from "./lib/utils/has-error-code"
 import { isError } from "./lib/utils/is-error"
@@ -7,11 +8,15 @@ export { useCardano } from "./contexts/CardanoContext"
 export { CardanoToaster } from "./components/CardanoToaster"
 export { CardanoWalletSelector } from "./components/CardanoWalletSelector"
 
+export { useIsConnectedToTheCorrectNetwork } from "./hooks/use-is-connected-to-the-correct-network"
+
+
 export const utility = {
   toNetworkId,
   toNetworkName,
   hasErrorCode,
   isError,
+  shortAddress
 }
 
 export const constants = {
